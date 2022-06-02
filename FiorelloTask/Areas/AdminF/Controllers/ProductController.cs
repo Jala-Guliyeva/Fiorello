a5950f5b7a3322d440ab0a55f99f5150e6ed328c
Jalə Quliyeva <quliyevajale52@gmail.com>
2022-06-02 22:49:42 +0400

    feat:Pagination

--- FiorelloTask/Areas/AdminF/Controllers/ProductController.cs
+++ FiorelloTask/Areas/AdminF/Controllers/ProductController.cs
@@ -1,5 +1,11 @@
 ﻿using FiorelloTask.DAL;
+using FiorelloTask.Models;
+using FiorelloTask.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FiorelloTask.Areas.AdminF.Controllers
 {
@@ -12,9 +18,61 @@ namespace FiorelloTask.Areas.AdminF.Controllers
                 _context = context;
 
             }
-            public IActionResult Index()
+            public IActionResult Index(int take=5,int pageSize=1)
         {
-            return View();
+            List<Product> products = _context.Products
+                .Include(p => p.Category)
+                .Skip((pageSize-1)*take)
+                .Take(take)
+                .ToList();
+
+            Pagination<ProductVM> pagination = new Pagination<ProductVM>
+            (
+                   ReturnPageCount(take),
+                    pageSize,
+                MapProductToProductVM(products)
+             
+                
+            );
+            return View(pagination);
+        }
+        private List<ProductVM> MapProductToProductVM(List<Product> products)
+        {
+            //List<ProductVM> productVMs = new List<ProductVM>();
+
+            //foreach (var item in products)
+            //{
+            //    ProductVM product = new ProductVM
+            //    {   
+            //        Id=item.Id,
+            //        Name = item.Name,
+            //        ImageUrl=item.ImageUrl,
+            //        Count=item.Count,
+            //        Price=item.Price,
+            //        CategoryName=item.Category.Name
+
+
+            //    };
+            //    productVMs.Add(product);
+            //}return productVMs;
+
+            List<ProductVM> productVMs = products.Select(p => new ProductVM
+            {
+                Id=p.Id,
+                Name=p.Name,
+                ImageUrl=p.ImageUrl,
+                Count=p.Count,
+                Price=p.Price,
+                CategoryName=p.Category.Name
+            }).ToList();
+            return productVMs;
+        }
+
+        private int ReturnPageCount(int take)
+        {
+         int productCount = _context.Products.Count();
+
+            return (int)Math.Ceiling(((decimal)productCount/take));
         }
     }
 }
---
+++ FiorelloTask/ViewModels/Pagination.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace FiorelloTask.ViewModels
+{
+    public class Pagination<T>
+    {
+        public int PageCount { get; set; }
+        public int CurrentPage { get; set; }
+        public List <T>  Items { get; set; }
+
+        public Pagination(int pagecount,int currentpage,List<T>items)
+        {
+            PageCount=pagecount;
+            CurrentPage=currentpage;
+            Items=items;
+        }
+    }
+}
